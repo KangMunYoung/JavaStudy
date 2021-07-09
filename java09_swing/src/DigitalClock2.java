eec36a9d1c9d6407bb5b1f67c1df184355c15cd4
KangMunYoung <kangtb321@gmail.com>
2021-07-09 17:43:33 +0900

    올리기

--- java09_swing/src/DigitalClock2.java
+++ java09_swing/src/DigitalClock2.java
@@ -2,47 +2,49 @@ import java.util.Calendar;
 import java.awt.Font;
 import java.text.SimpleDateFormat;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class DigitalClock2 extends JFrame{
-	Font fnt = new Font("굴림", Font.BOLD, 100);
-	Calendar now;
-	JLabel lbl = new JLabel("00:00:00", JLabel.CENTER);
+public class DigitalClock extends JFrame {
+   Font fnt = new Font("굴림", Font.BOLD, 100);
+   Calendar now;
+   JLabel lbl = new JLabel("00:00:00", JLabel.CENTER);//가운데 정렬 안하면 왼쪽에 찍힘
+   ImageIcon icon = new ImageIcon("img/th.jpg");
 
-	public DigitalClock2() {
-		super("시계"); //setTitle("�떆怨�");
-		add(lbl);
-		lbl.setFont(fnt);
-		
-		setSize(500, 200);
-		setVisible(true);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		startClock();
-		
-	}
-	
-	//�떆媛꾨�寃�
-	public void startClock() {
-		do {
-			//�떆媛꾨�寃�
-			lbl.setText(getTimer());
-			try {
-				Thread.sleep(1000);
-			}catch(InterruptedException e) {
-				
-			}
-		}while(true);
-	}
-	//�쁽�옱�떆媛�(�떆遺꾩큹) 00:00:00
-	public String getTimer() {
-		now = Calendar.getInstance();
-		SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
-		return fmt.format(now.getTime());
-	}
-	
-	public static void main(String[] args) {
-		new DigitalClock2();
-	}
+   public DigitalClock() {   
+      super("CLOCK");
+      add(lbl);
+      lbl.setFont(fnt);
+      
+      setSize(500,200);
+      setVisible(true);
+      
+      startClock();
+   }
+   //시간을 변경
+   public void startClock() {
+      do {
+         //시간을 변경
+         lbl.setText(getTimer());
+         try {
+            Thread.sleep(1000);
+         }catch(InterruptedException e) {
+            
+         }
+      }while(true);
+   }
+   //현재시간(시분초)을 00:00:00
+   public String getTimer() {
+      now = Calendar.getInstance();
+      
+      SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss");//대문자  H 13시 이런식으로 찍힘
+      return fmt.format(now.getTime());
+   
+   }
+   public static void main(String[] args) {
+      new DigitalClock();
+
+   }
 
 }
