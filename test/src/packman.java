fa197a02097c4e4599825d2724a2101ba09ff125
KangMunYoung <kangtb321@gmail.com>
2021-07-09 09:55:15 +0900

    팩맨팩맨!

--- test/src/packman.java
+++ test/src/packman.java
@@ -10,78 +10,74 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class Packman extends JFrame implements KeyListener{
+public class packman extends JFrame implements KeyListener{
+	JPanel pane = new JPanel();
 	
 	Image img;
 	MyCanvas mc = new MyCanvas();
-	int x = 200;
+	int x = 430;
 	int y = 200;
 	int imgX1 = 0;
 	int imgX2 = 1+imgX1;
-	
-	public Packman() {
+	int re;
+	public packman() {
 		setBackground(Color.WHITE);
 		setSize(500,500);
-		setVisible(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		addKeyListener(this);
 		update(getGraphics());
-		add(mc);
+		add("Center",mc);
+		setVisible(true);
 		while(true) {
-//			mc.repaint();
-			try {
-				if(imgX1==1 || imgX1 ==0) {
-					do {
+		   try {
+			    mc.repaint();
+							
+		  if(re==1) { // while문으로 변경 좌표 기준해서 다시 작성
+			
+			  brekaout:
+				while(true){
+				
+					for(int i =0; i< 90; i++) {				
 						mc.repaint();
-						x-=5;
-						if(imgX1==1) {
-							imgX1 =0;
-						}else if(imgX1==0) {
-							imgX1 =1;
-						}
+						x -=5;
 						Thread.sleep(100);
-					}while(imgX1==1 || imgX1 ==0);
-					
-				}else if(imgX1==2 || imgX1==3) {
-					do {
-						mc.repaint();
-						x+=5;
-						if(imgX1==2) {
-							imgX1 =3;
-						}else if(imgX1==3) {
-							imgX1 =2;
-						}
-						Thread.sleep(100);
-					}while(imgX1==2 || imgX1==3);
-					
-				}else if(imgX1==4 || imgX1==5) {
-					do {
-						mc.repaint();
-						y-=5;
-						if(imgX1==4) {
-							imgX1 =5;
-						}else if(imgX1==5) {
-							imgX1 =4;
-						}
-						Thread.sleep(100);
-					}while(imgX1==4 || imgX1==5);
-					
-				}else if(imgX1==6 || imgX1==7) {
-					do {
-						mc.repaint();
-						y+=5;
-						if(imgX1==6) {
-							imgX1 =7;
-						}else if(imgX1==7) {
-							imgX1 =6;
-						}
-						Thread.sleep(100);
-					}while(imgX1==6 || imgX1==7);
-					
+					if(imgX1 == 0) {
+						imgX1 = 1;
+					}else if(imgX1 == 1) {
+						imgX1 = 0;
+					}else {
+						imgX1=0;
+					}
+					if(re==0) {
+						break brekaout;
+					}
+			 			}
+			
+		for(int j =0; j < 90; j++) {
+					mc.repaint();
+					x +=5;
+					Thread.sleep(100);
+					if(imgX1 == 2) {
+						imgX1 = 3;
+					}else if(imgX1 == 3) {
+						imgX1 = 2;
+					}else {
+						imgX1 = 2;
+					}
+					if(re==0) {
+						break brekaout;
+					}
+				
+			}
+				
 				}
-				Thread.sleep(100);
-			}catch(Exception e) {}
+							}
+			}catch(Exception e){
+				
 		}
+	
+		}
+		
 	}
 	
 		
@@ -97,7 +93,22 @@ public class Packman extends JFrame implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		do {
+			
+		// 스페이스 버튼 추가 
+			if(key == KeyEvent.VK_SPACE) {
+				System.out.println(key);
+				if(re==1) {
+					re=0;
+					break;
+				}
+				re = 1;
+				break;
+			}	
+			
 			if(key == KeyEvent.VK_LEFT) {
+				System.out.println(key);
+				x -= 5;
+				System.out.println(x);
 				if(imgX1 == 0) {
 					imgX1 = 1;
 				}else if(imgX1 == 1) {
@@ -107,6 +118,8 @@ public class Packman extends JFrame implements KeyListener{
 				}
 				break;
 			}else if(key == KeyEvent.VK_RIGHT) {
+				System.out.println(key);
+				x += 5;
 				if(imgX1 == 2) {
 					imgX1 = 3;
 				}else if(imgX1 == 3) {
@@ -117,6 +130,8 @@ public class Packman extends JFrame implements KeyListener{
 				break;
 				
 			}else if(key == KeyEvent.VK_UP) {
+				System.out.println(key);
+				y -= 5;
 				if(imgX1 == 4) {
 					imgX1 = 5;
 				}else if(imgX1 == 5) {
@@ -125,8 +140,9 @@ public class Packman extends JFrame implements KeyListener{
 					imgX1 = 4;
 				}
 				break;
-				
 			}else if(key == KeyEvent.VK_DOWN) {
+				System.out.println(key);
+				y += 5;
 				if(imgX1 == 6) {
 					imgX1 = 7;
 				}else if(imgX1 == 7) {
@@ -144,7 +160,7 @@ public class Packman extends JFrame implements KeyListener{
 	public void keyReleased(KeyEvent e) {}
 	
 	public static void main(String[] args) {
-		new Packman();
+		new packman();
 	}
 
 }
